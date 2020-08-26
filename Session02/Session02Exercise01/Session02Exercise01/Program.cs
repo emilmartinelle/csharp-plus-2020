e13780fc53c05020945638354b2058db33693bf5
DESKTOP-R1476QT\Emil <emil.martinelle@studerande.plushhogskolan.se>
2020-08-26 10:38:47 +0200

    string test

--- Session02/Session02Exercise01/Session02Exercise01/Program.cs
+++ Session02/Session02Exercise01/Session02Exercise01/Program.cs
@@ -7,6 +7,15 @@ namespace Session02Exercise01
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            var integer = 0;
+
+            Console.WriteLine("Integer is " + integer.ToString());
         }
     }
 }
+
+
+string value = "MyStringValue";
+
+Console.WriteLine("The value of stringValue is: " + stringValue);
