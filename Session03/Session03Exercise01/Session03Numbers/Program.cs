c7d891bf176d5b868750d0fab349059a2ed0414e
Emil Martinelle <emil.martinelle@studerande.plushhogskolan.se>
2020-08-28 09:36:29 +0200

    numbers

---
+++ Session03/Session03Exercise01/Session03Numbers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Session03Numbers
+{
+    using System;
+
+    namespace Session03Exercise02
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Ange ett antal siffror, separat med kommatecken");
+
+                var input = Console.ReadLine();
+
+                var inputArray = input.Split(",", StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (var number in inputArray)
+                {
+                    Console.WriteLine("Värdet är " + number);
+                }
+            }
+        }
+    }
